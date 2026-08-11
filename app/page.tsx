f24122696011e1e408d68830df9ebc0487f78cfd
zcdev <zcdev@hotmail.com>
2026-08-11 13:21:46 -0700

    chore(init): set up Next.js for project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center font-sans">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+
+      </main>
+    </div>
+  );
+}
